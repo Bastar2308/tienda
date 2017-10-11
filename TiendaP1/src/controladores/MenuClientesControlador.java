ee9dd3284b3db97a6b4a27650cb1ff794b3ef706
bastar2308 <bastar_cg@hotmail.com>
2017-10-11 18:35:58 -0500

    último de la tarde

--- TiendaP1/src/controladores/MenuClientesControlador.java
+++ TiendaP1/src/controladores/MenuClientesControlador.java
@@ -6,7 +6,8 @@
 package controladores;
 
 import auxiliares.GuiTools;
-import gui.JfMenuCategorias;
+import gui.JfMenuClientes;
+import gui.JfMenuMarcas;
 import gui.JfMenuPrincipal;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,17 +16,23 @@ import java.awt.event.ActionListener;
  *
  * @author MAESTROAD
  */
-public class MenuCategoriasControlador implements ActionListener{
-    JfMenuCategorias vista;
+public class MenuClientesControlador implements ActionListener{
 
-    public MenuCategoriasControlador(JfMenuCategorias vista) {
+    JfMenuClientes vista;
+
+    public MenuClientesControlador(JfMenuClientes vista) {
         this.vista = vista;
+        cargarListeners();
     }
-    public void addListeners(){
-    
+
+    private void cargarListeners() {
+        vista.getJbRegresar().addActionListener(this);
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-       
+        if (e.getSource().equals(vista.getJbRegresar())) {
+            GuiTools.getInstance().abre(vista, new JfMenuPrincipal());
+        }
     }
 }
