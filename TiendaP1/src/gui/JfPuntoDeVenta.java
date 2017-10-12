e88afad3cb8f818c059eca6ae824a51463ab5eed
DavidVazquez <burnkillreach@gmail.com>
2017-10-12 11:11:15 -0500

    Termina interfaz lógica y gráfica de punto de venta e impleméntala

--- TiendaP1/src/gui/JfPuntoDeVenta.java
+++ TiendaP1/src/gui/JfPuntoDeVenta.java
@@ -33,27 +33,172 @@ public class JfPuntoDeVenta extends javax.swing.JFrame implements JfPuntoDeVenta
     private void initComponents() {
 
         jbRegresar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtDatos = new javax.swing.JTable();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jlTotal = new javax.swing.JLabel();
+        jlProductos = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jbClientes = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        tfBuscar = new javax.swing.JTextField();
+        jbFiltrar = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jlClientes = new javax.swing.JList<>();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jbRegresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/Import_16px.png"))); // NOI18N
         jbRegresar.setText("Regresar");
 
+        jtDatos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                { new Boolean(true), null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                { new Boolean(true), null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "Selección", "Nombre", "Precio", "Stock", "Detalles", "Categoría", "Marca", "Código"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Boolean.class, java.lang.String.class, java.lang.Double.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                true, false, false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jtDatos.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(jtDatos);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Total"));
+
+        jLabel1.setText("Total:");
+
+        jlTotal.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jlTotal.setText("99999.99999");
+
+        jlProductos.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jlProductos.setText("9999999999");
+
+        jLabel4.setText("Productos:");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jlTotal))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                        .addComponent(jlProductos)))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jlTotal))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jlProductos))
+                .addContainerGap(178, Short.MAX_VALUE))
+        );
+
+        jbClientes.setText("Confirmar venta");
+
+        jLabel5.setText("Buscar:");
+
+        jbFiltrar.setText("Filtrar");
+
+        jlClientes.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jlClientes.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jScrollPane2.setViewportView(jlClientes);
+
+        jLabel6.setText("Productos");
+
+        jLabel7.setText("Clientes");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(293, Short.MAX_VALUE)
-                .addComponent(jbRegresar)
-                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel7)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jbRegresar))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(tfBuscar)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jbFiltrar))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(jbClientes))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 507, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(22, 22, 22))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jbRegresar)
-                .addContainerGap(264, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbRegresar)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(tfBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jbFiltrar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 241, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jbClientes)
+                .addContainerGap())
         );
 
         pack();
@@ -95,7 +240,22 @@ public class JfPuntoDeVenta extends javax.swing.JFrame implements JfPuntoDeVenta
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JButton jbClientes;
+    private javax.swing.JButton jbFiltrar;
     private javax.swing.JButton jbRegresar;
+    private javax.swing.JList<String> jlClientes;
+    private javax.swing.JLabel jlProductos;
+    private javax.swing.JLabel jlTotal;
+    private javax.swing.JTable jtDatos;
+    private javax.swing.JTextField tfBuscar;
     // End of variables declaration//GEN-END:variables
 
     @Override
@@ -107,4 +267,61 @@ public class JfPuntoDeVenta extends javax.swing.JFrame implements JfPuntoDeVenta
     public void setJbRegresar(JButton jb) {
         this.jbRegresar = jb;
     }
+
+    public javax.swing.JButton getJbClientes() {
+        return jbClientes;
+    }
+
+    public void setJbClientes(javax.swing.JButton jbClientes) {
+        this.jbClientes = jbClientes;
+    }
+
+    public javax.swing.JButton getJbFiltrar() {
+        return jbFiltrar;
+    }
+
+    public void setJbFiltrar(javax.swing.JButton jbFiltrar) {
+        this.jbFiltrar = jbFiltrar;
+    }
+
+    public javax.swing.JList<String> getJlClientes() {
+        return jlClientes;
+    }
+
+    public void setJlClientes(javax.swing.JList jlClientes) {
+        this.jlClientes = jlClientes;
+    }
+
+    public javax.swing.JLabel getJlProductos() {
+        return jlProductos;
+    }
+
+    public void setJlProductos(javax.swing.JLabel jlProductos) {
+        this.jlProductos = jlProductos;
+    }
+
+    public javax.swing.JLabel getJlTotal() {
+        return jlTotal;
+    }
+
+    public void setJlTotal(javax.swing.JLabel jlTotal) {
+        this.jlTotal = jlTotal;
+    }
+
+    public javax.swing.JTable getJtDatos() {
+        return jtDatos;
+    }
+
+    public void setJtDatos(javax.swing.JTable jtDatos) {
+        this.jtDatos = jtDatos;
+    }
+
+    public javax.swing.JTextField getTfBuscar() {
+        return tfBuscar;
+    }
+
+    public void setTfBuscar(javax.swing.JTextField tfBuscar) {
+        this.tfBuscar = tfBuscar;
+    }
+    
 }
