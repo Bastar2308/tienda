c36bc8aa9b1942238eb8c4b1cb9550818c4119ef
DavidVazquez <burnkillreach@gmail.com>
2017-10-17 16:08:39 -0500

    Agrega if de control de grupos

---
+++ TiendaP1/src/guiif/JfControlDeGruposIf.java
@@ -0,0 +1,63 @@
+/*
+ * Coded by David Vazquez using NetBeans.
+ */
+package guiif;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author BurnKill
+ */
+public interface JfControlDeGruposIf {
+    public abstract JTable getJtDatos();
+public abstract void setJtDatos(JTable x);
+
+public abstract JButton getJbAgregar();
+public abstract void setJbAgregar(JButton x);
+
+public abstract JButton getJbEditar();
+public abstract void setJbEditar(JButton x);
+
+public abstract JButton getJbRegresar();
+public abstract void setJbRegresar(JButton x);
+
+public abstract JButton getJbEliminar();
+public abstract void setJbEliminar(JButton x);
+
+public abstract JTextField getTfAgregarNombre();
+public abstract void setTfAgregarNombre(JTextField x);
+
+public abstract JTextField getTfAgregarNivel();
+public abstract void setTfAgregarNivel(JTextField x);
+
+public abstract JButton getJbAgregarCancelar();
+public abstract void setJbAgregarCancelar(JButton x);
+
+public abstract JButton getJbAgregarAceptar();
+public abstract void setJbAgregarAceptar(JButton x);
+
+public abstract JLabel getJlGrupo();
+public abstract void setJlGrupo(JLabel x);
+
+public abstract JButton getJbEliminarCancelar();
+public abstract void setJbEliminarCancelar(JButton x);
+
+public abstract JButton getJbEliminarAceptar();
+public abstract void setJbEliminarAceptar(JButton x);
+
+public abstract JTextField getTfEditarNombre();
+public abstract void setTfEditarNombre(JTextField x);
+
+public abstract JTextField getTfEditarNivel();
+public abstract void setTfEditarNivel(JTextField x);
+
+public abstract JButton getJbEditarCancelar();
+public abstract void setJbEditarCancelar(JButton x);
+
+public abstract JButton getJbEditarAceptar();
+public abstract void setJbEditarAceptar(JButton x);
+}
