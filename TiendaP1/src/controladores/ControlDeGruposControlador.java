eed14cc52c3780adf78de5193a47ba3c20d2cd5a
MAESTROAD <MAESTROAD@MAESTRO-AD>
2017-10-18 08:37:02 -0500

    Crear control de grupos controlador

---
+++ TiendaP1/src/controladores/ControlDeGruposControlador.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import gui.JfControlDeGrupos;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author MAESTROAD
+ */
+public class ControlDeGruposControlador implements ActionListener {
+
+    JfControlDeGrupos vista;
+
+    public ControlDeGruposControlador(JfControlDeGrupos vista) {
+        this.vista = vista;
+    }
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
