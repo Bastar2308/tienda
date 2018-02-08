47b2d87501f9892209a9c796f2b793a8a92f7243
David Vazquez <burnkillreach@gmail.com>
2018-02-08 09:08:10 -0600

    Carga tabla de productos en punto de venta

--- TiendaP1/src/controladores/PuntoDeVentaControlador.java
+++ TiendaP1/src/controladores/PuntoDeVentaControlador.java
@@ -6,10 +6,12 @@
 package controladores;
 
 import auxiliares.GuiTools;
+import dao.ProductoDAO;
 import gui.JfMenuPrincipal;
 import gui.JfPuntoDeVenta;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -20,9 +22,9 @@ public class PuntoDeVentaControlador implements ActionListener {
     JfPuntoDeVenta vista;
 
     public PuntoDeVentaControlador(JfPuntoDeVenta vista) {
-        this.vista = vista;
+        this.vista=vista;
         addListeners();
-
+        cargaTabla();
     }
 
     private void addListeners() {
@@ -33,6 +35,15 @@ public class PuntoDeVentaControlador implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(vista.getJbRegresar())) {
             GuiTools.getInstance().regresaMenu(vista);
+        }
+    }
+
+    private void cargaTabla() {
+        DefaultTableModel original=(DefaultTableModel) vista.getJtProductos().getModel();
+        original.setRowCount(0);
+        DefaultTableModel datos=ProductoDAO.getInstance().cargarTabla();
+        for (int i=0; i<datos.getRowCount(); i++) {
+            original.addRow(new Object[]{datos.getValueAt(i, 0), datos.getValueAt(i, 1), datos.getValueAt(i, 2), datos.getValueAt(i, 3), datos.getValueAt(i, 4), datos.getValueAt(i, 5), datos.getValueAt(i, 6), datos.getValueAt(i, 7)});
         }
     }
 
