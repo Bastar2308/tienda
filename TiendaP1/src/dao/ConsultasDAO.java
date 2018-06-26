9d5553bba4d94bd129283505667ab0c59002ab04
David Vazquez <vvdavided@gmail.com>
2018-06-26 16:37:33 -0500

    Agrega método para seleccionar rango de compras

--- TiendaP1/src/dao/ConsultasDAO.java
+++ TiendaP1/src/dao/ConsultasDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import auxiliares.GuiTools;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import javax.swing.table.DefaultTableModel;
@@ -17,6 +18,7 @@ public class ConsultasDAO {
     public static final String VENTAS_POR_CATEGORIA = "";
     public static final String VENTAS_POR_MARCA = "";
     public static final String VENTAS_POR_PRODUCTO = "";
+    public static final String VENTAS_EN_RANGO = "";
 
     private ConsultasDAO() {
     }
@@ -40,7 +42,27 @@ public class ConsultasDAO {
             ResultSet rs = st.executeQuery();
             dt = GuiTools.getInstance().resultSetToDefaultTableModel(rs);
         } catch (Exception e) {
-            System.out.println("Error al cargar la tabla marca " + e);
+            System.out.println("Error al cargar la consulta" + e);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(st);
+        }
+        return dt;
+    }
+
+    public DefaultTableModel consultaComprasEnRango(Date desde, Date hasta) {
+        Connection con = null;
+        PreparedStatement st = null;
+        DefaultTableModel dt = new DefaultTableModel();
+        try {
+            con = Conexion.getConnection();
+            st = con.prepareStatement(VENTAS_EN_RANGO);
+            st.setDate(1, desde);
+            st.setDate(2, hasta);
+            ResultSet rs = st.executeQuery();
+            dt = GuiTools.getInstance().resultSetToDefaultTableModel(rs);
+        } catch (Exception e) {
+            System.out.println("Error al cargar la consulta" + e);
         } finally {
             Conexion.close(con);
             Conexion.close(st);
