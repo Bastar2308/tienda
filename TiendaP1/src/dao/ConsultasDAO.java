9bbcc17cec34af209888b53c20c7552eed9452d2
Aaron160399 <aaronstone68@hotmail.com>
2018-06-28 00:56:19 -0500

    Consultas

--- TiendaP1/src/dao/ConsultasDAO.java
+++ TiendaP1/src/dao/ConsultasDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.swing.table.DefaultTableModel;
 import pojo.Cliente;
 
@@ -16,10 +17,16 @@ import pojo.Cliente;
  */
 public class ConsultasDAO {
 
-    public static final String VENTAS_POR_CATEGORIA = "";
-    public static final String VENTAS_POR_MARCA = "";
-    public static final String VENTAS_POR_PRODUCTO = "";
-    public static final String VENTAS_EN_RANGO = "";
+    public static final String VENTAS_POR_CATEGORIA = "SELECT categoria.nombre AS Categoria, COUNT(categoria.nombre) AS Total FROM detalle_venta\n" +
+"INNER JOIN producto ON detalle_venta.Producto_idProducto =  producto.idProducto\n" +
+"INNER JOIN categoria ON producto.Categoria_idCategoria = categoria.idCategoria\n" +
+"GROUP BY categoria.nombre";
+    public static final String VENTAS_POR_MARCA = "SELECT marca.nombre, COUNT(marca.nombre) AS Marca FROM detalle_venta\n" +
+"INNER JOIN producto ON detalle_venta.Producto_idProducto =  producto.idProducto\n" +
+"INNER JOIN marca ON producto.Marca_idMarca = marca.idMarca\n" +
+"GROUP BY marca.nombre";
+    public static final String VENTAS_POR_PRODUCTO = "SELECT detalle_venta.Producto_idProducto, SUM(detalle_venta.cantidad) AS 'Cantidad total' FROM detalle_venta GROUP BY detalle_venta.Producto_idProducto";
+    public static final String VENTAS_EN_RANGO = "SELECT * FROM venta WHERE venta.fechahora BETWEEN =? AND =?";
 
     private ConsultasDAO() {
     }
@@ -36,10 +43,11 @@ public class ConsultasDAO {
     public DefaultTableModel consulta(String consulta) {
         Connection con = null;
         PreparedStatement st = null;
+        Statement st2 = null;
         DefaultTableModel dt = new DefaultTableModel();
         try {
             con = Conexion.getConnection();
-            st = con.prepareStatement(consulta);
+            st2 = con.prepareStatement(consulta);
             ResultSet rs = st.executeQuery();
             dt = GuiTools.getInstance().resultSetToDefaultTableModel(rs);
         } catch (Exception e) {
