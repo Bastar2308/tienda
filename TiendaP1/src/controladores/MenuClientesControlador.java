bbc4ddd10e42dcc3622c1c4e679c4b003e55d7a3
David Vazquez <vvdavided@gmail.com>
2018-08-13 21:49:35 -0500

    Actualiza tabla de clientes al regresar

--- TiendaP1/src/controladores/MenuClientesControlador.java
+++ TiendaP1/src/controladores/MenuClientesControlador.java
@@ -19,6 +19,8 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
@@ -59,6 +61,12 @@ public class MenuClientesControlador implements ActionListener {
         addListeners();
         cargarTabla();
         cargarCombos();
+        vista.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentShown(ComponentEvent e) {
+                cargarTabla();
+            }
+        });
     }
 
     public void cargarTabla() {
