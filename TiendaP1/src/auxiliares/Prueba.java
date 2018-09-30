55c262bf2ae5701e1da874f75653be3166a58b0b
David Vazquez <vvdavided@gmail.com>
2018-09-30 12:48:50 -0500

    Crea configuración

--- TiendaP1/src/auxiliares/Prueba.java
+++ TiendaP1/src/auxiliares/Prueba.java
@@ -11,10 +11,18 @@ package auxiliares;
  */
 public class Prueba {
 
+    public static int NEGOCIO = 1;
+    public static int ESCUELA = 2;
+    public static int ENCARGADO = 3;
+    public static int TELEFONO = 4;
+    public static int DIRECCION = 5;
+
     public static void main(String[] args) {
-        Prefs.put("pr", "DD");
-        System.out.println("El valor es: " + Prefs.get("pr"));
-        Prefs.put("pr", "AA");
-        System.out.println("El valor es: " + Prefs.get("pr"));
+        Prefs.put(NEGOCIO, "Ccafeteria Generica");
+        Prefs.put(ESCUELA, "Baltico");
+        Prefs.put(ENCARGADO, "Raquel Perez");
+        Prefs.put(TELEFONO, "22-25925922259");
+        Prefs.put(DIRECCION, "Costa de oro");
+//        System.out.println("El valor es: " + Prefs.get("pr"));
     }
 }
