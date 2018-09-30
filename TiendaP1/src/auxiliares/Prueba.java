b80ca68767b836b6cb053e153e1258966ab7565d
David Vazquez <vvdavided@gmail.com>
2018-09-30 12:42:38 -0500

    Agrega clase de prueba

---
+++ TiendaP1/src/auxiliares/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auxiliares;
+
+/**
+ *
+ * @author Admin
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        Prefs.put("pr", "DD");
+        System.out.println("El valor es: " + Prefs.get("pr"));
+        Prefs.put("pr", "AA");
+        System.out.println("El valor es: " + Prefs.get("pr"));
+    }
+}
